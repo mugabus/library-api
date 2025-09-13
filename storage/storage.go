eb047016aa8041225cfb039729c651b2db0de0d0
unknown <mugabus>
2025-09-13 05:36:01 -0700

    done storage

---
+++ storage/storage.go
@@ -0,0 +1,52 @@
+package storage
+
+import (
+	"errors"
+	"library-api/models"
+)
+
+// in memory storage
+
+var Books = make(map[int]models.Book)
+var Members = make(map[int]models.Member)
+var NextBookID = 1
+var NextMemberID = 1
+
+func addBook(book models.Book) models.Book {
+	book.BookID = NextBookID
+	Books[NextBookID] = book
+	NextBookID++
+	return book
+}
+func addMember(member models.Member) models.Member {
+	member.MemberID = NextMemberID
+	Members[NextMemberID] = member
+	NextMemberID++
+	return member
+}
+
+// Borrow book
+func borrowBook(bookID int) error {
+	book, exists := Books[bookID]
+	if !exists {
+		return errors.New("book not found")
+	}
+	if book.Borrowed {
+		return errors.New("book already borrowed")
+	}
+	book.Borrowed = true
+	Books[bookID] = book
+	return nil
+}
+func ReturnBook(bookID int) error {
+	book, exists := Books[bookID]
+	if !exists {
+		return errors.New("book not foound")
+	}
+	if !book.Borrowed {
+		return errors.New("book was not borrowed")
+	}
+	book.Borrowed = false
+	Books[bookID] = book
+	return nil
+}
